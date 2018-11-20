c6fe5936b40c46217769e707cccb11c0a11d9e79
Thomas Dufourd <thomas.dufourd@gmail.com>
2018-11-21 00:18:46 +0100

    [ex2] adding exercices (tests) to build the robotquest

---
+++ ex2/src/robotquest-features.js
@@ -0,0 +1,69 @@
+module.exports =
+    {turn, move, updateBoard, checkIfFlagReached, cloneRobot};
+
+const SYMBOLS = {
+    robot: setReverse('R'),
+    tree: colorInGreen('T'),
+    flag: colorInYellow('F'),
+    water: setBlueBg('w')
+
+}
+module.exports.SYMBOLS = SYMBOLS;
+
+const trailIndicators = {
+    left: setBright('←'),
+    right: setBright('→'),
+    up: setBright('↑'),
+    down: setBright('↓')
+};
+
+
+function turn(robot, step, turns) {
+    turns += 1;
+    return turns;
+}
+
+function move(robot, maxLineIndex, maxColumnIndex, nbOfMoves) {
+    nbOfMoves += 1;
+    return nbOfMoves;
+}
+
+function updateBoard(board, previousRobotState, currentRobotState) {
+    return;
+}
+
+function checkIfFlagReached(robot, board) {
+    return false;
+}
+
+// utils
+function cloneRobot(robot) {
+    let newRobot = {};
+    newRobot.position = {};
+    newRobot.position.line = robot.position.line;
+    newRobot.position.column = robot.position.column;
+    newRobot.head = robot.head;
+    return newRobot;
+}
+
+
+// presentation utils
+function colorInGreen(char) {
+    return `\x1b[32m${char}\x1b[0m`;
+}
+
+function colorInYellow(char) {
+    return `\x1b[33m${char}\x1b[0m`;
+}
+
+function setBlueBg(char) {
+    return `\x1b[44m${char}\x1b[0m`;
+}
+
+function setReverse(char) {
+    return `\x1b[7m${char}\x1b[0m`;
+}
+
+function setBright(char) {
+    return `\x1b[1m${char}\x1b[0m`;
+}
